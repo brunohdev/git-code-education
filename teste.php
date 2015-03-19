10ab250ef7c9f2567f1f85226b7f7252138b8ee5
Bruno Henrique <brunohdev@gmail.com>
2015-03-19 16:54:09 -0300

    Corrigindo Ola com informações de contato

--- teste.php
+++ teste.php
@@ -1,2 +1,11 @@
-<?php phpinfo(); ?>
-Ola!
+<?php
+phpinfo();
+
+$nome = "Bruno Henrique";
+$email = "brunohdev@gmail.com";
+$github = "https://github.com/brunohdev/";
+
+echo "Olá {$nome}!<br/><br/>";
+echo "Estas são suas informações de contato:<br/>";
+echo "{$email} # {$github}";
+?>
